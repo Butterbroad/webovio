09a203a52840d1a1a763d756819a2e0d7cc455fd
Butterbroad <pyatochevskiy@gmail.com>
2020-04-01 20:03:04 +0300

    finished md adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,15 @@
-$(function () {
+$(function () { }
 
+);
 
-
-
-});
-
+const header = document.querySelector('.header');
 const headerBurger = document.querySelector('.header__burger');
+const headerMenu = document.querySelector('.header__menu');
+const bodyLock = document.querySelector('body');
 
 headerBurger.addEventListener('click', function () {
     headerBurger.classList.toggle('active');
+    headerMenu.classList.toggle('active');
+    bodyLock.classList.toggle('lock');
+    header.classList.toggle('active');
 });
